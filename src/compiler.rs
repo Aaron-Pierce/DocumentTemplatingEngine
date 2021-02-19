2e20cf1fd9b8b3f1282aa54cfbc2600592e5a203
Aaron <aaronmp01@gmail.com>
2021-02-18 19:01:41 -0600

    refactored a little

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,17 +1,55 @@
-use std::{fs::read_to_string, path::Path};
+use std::{env::Args, fs::read_to_string, path::Path};
 
-pub struct Compiler<'a>{
+use fs_extra::dir::CopyOptions;
+
+pub struct Compiler<'a> {
     pub template_directory: &'a str,
-    pub article_directory: &'a str
+    pub article_directory: &'a str,
 }
 
-impl<'a> Compiler<'a>{
-    pub fn compile(&self, name: &String) {
+impl<'a> Compiler<'a> {
+    // Parses out commands and runs the corresponding methods
+    pub fn interpret_arguments(&self, args: &mut Args) {
+        if args.len() > 0 {
+            let command = args.next().unwrap().to_lowercase();
+            if command == "compile" {
+                args.for_each(|e| {
+                    if e == "all" {
+                        self.compile_all();
+                    } else {
+                        self.compile(&e);
+                    }
+                });
+            } else if command == "publish" {
+                self.compile_all();
+                self.publish();
+            }
+        }
+    }
+
+    pub fn compile_all(&self) {
+        std::fs::read_dir(self.article_directory)
+            .unwrap()
+            .for_each(|file_result| {
+                if file_result.is_ok() {
+                    let file_name = file_result.unwrap().file_name();
+                    match file_name.to_string_lossy().strip_suffix(".html") {
+                        Some(filename) => self.compile(&filename.to_string()),
+                        None => {}
+                    }
+                }
+            });
+        
+        self.copy_static_files();
+        println!("Compiled all templates.")
+    }
+
+    pub fn compile(&self, name: &str) {
         use std::fs::File;
         use std::io::Write;
-    
+
         println!("Requested compile of {:?}", name);
-    
+
         let built_article = self.build_article(name);
         let build_path = Path::new("./built/");
         match std::fs::create_dir(build_path) {
@@ -33,39 +71,82 @@ impl<'a> Compiler<'a>{
             .expect("Could not write file. Do you have write permissions?");
     }
 
+    pub fn copy_static_files(&self) {
+        let copy_options = CopyOptions {
+            overwrite: true,
+            skip_exist: false,
+            content_only: true,
+            ..CopyOptions::new()
+        };
+
+        println!("Copying static directory contents to build directory...");
+
+        fs_extra::dir::copy("./src/public", "./built", &copy_options)
+            .expect("Could not copy contents of ./src/public");
+    }
+
+    fn publish(&self) {
+        std::process::Command::new("git")
+            .args(&["add", "."])
+            .current_dir("./built")
+            .status()
+            .expect("Failed to add all files");
+
+        std::process::Command::new("git")
+            .args(&["commit", "-m", "TODO: add a real commit message"])
+            .current_dir("./built")
+            .status()
+            .expect("Failed to commit");
+
+        std::process::Command::new("git")
+            .args(&["push", "origin", "main"])
+            .current_dir("./built")
+            .status()
+            .expect("Failed to push");
+
+        println!("Published")
+    }
+
     fn expand_bang(&self, bang_name: &str, args: Vec<String>) -> String {
         match bang_name {
             "makeTitle" => {
                 let mut template_data =
                     read_to_string(format!("{}{}", self.template_directory, "title.html")).unwrap();
-    
+
                 template_data = template_data.replace(
                     "*+*",
                     args.get(0).unwrap_or(&String::from("Error: Title Missing")),
                 );
-                template_data = template_data.replace("?*?", args.get(1).unwrap_or(&String::from("")));
-    
+                template_data =
+                    template_data.replace("?*?", args.get(1).unwrap_or(&String::from("")));
+
                 return template_data;
             }
             "archiveEntry" => {
-                let mut template_data =
-                    read_to_string(format!("{}{}", self.template_directory, "archiveEntry.html")).unwrap();
-    
-                template_data = template_data.replace("?+?", args.get(0).unwrap_or(&String::from("")));
-    
-                template_data = template_data.replace("*+*", args.get(1).unwrap_or(&String::from("")));
-    
-                template_data = template_data.replace("?d", args.get(2).unwrap_or(&String::from("")));
+                let mut template_data = read_to_string(format!(
+                    "{}{}",
+                    self.template_directory, "archiveEntry.html"
+                ))
+                .unwrap();
+
+                template_data =
+                    template_data.replace("?+?", args.get(0).unwrap_or(&String::from("")));
+
+                template_data =
+                    template_data.replace("*+*", args.get(1).unwrap_or(&String::from("")));
+
+                template_data =
+                    template_data.replace("?d", args.get(2).unwrap_or(&String::from("")));
                 return template_data;
             }
             _ => String::new(),
         }
     }
-    
-    fn build(&self, template_location: &String, reverse_template_content: Option<&String>) -> String {
-        println!("Built template request: {:#?}", template_location);
-        let template_data_result =read_to_string(template_location);
-    
+
+    fn build(&self, template_location: &str, reverse_template_content: Option<&str>) -> String {
+        println!("Building template... {:#?}", template_location);
+        let template_data_result = read_to_string(template_location);
+
         let mut template_data = match template_data_result {
             Ok(data) => data,
             Err(err) => {
@@ -80,15 +161,15 @@ impl<'a> Compiler<'a>{
         }
 
         let first_line = template_data.lines().next().unwrap();
-    
+
         let reverse_template: Option<String>;
         if first_line.starts_with("*") {
             reverse_template = Some(first_line.strip_prefix("*").unwrap().to_string());
-            println!(
-                "{} wants to be wrapped in {}",
-                template_location,
-                reverse_template.clone().unwrap()
-            );
+            // println!(
+            //     "{} wants to be wrapped in {}",
+            //     template_location,
+            //     reverse_template.clone().unwrap()
+            // );
             template_data = template_data.replace(first_line, "");
         } else {
             reverse_template = None;
@@ -96,13 +177,13 @@ impl<'a> Compiler<'a>{
 
         while template_data.contains("!{") {
             let parsed = self.parse_command(&template_data, "!{*}");
-    
+
             template_data.replace_range(
                 parsed.start_index..(parsed.start_index + parsed.total_length),
                 &self.expand_bang(&parsed.name[..], parsed.arguments)[..],
             );
         }
-    
+
         while template_data.contains("${") {
             let parsed = self.parse_command(&template_data, "${*}");
             if parsed.name.starts_with("-") && reverse_template_content.is_some() {
@@ -118,77 +199,41 @@ impl<'a> Compiler<'a>{
                 &self.build_template(&parsed.name, None)[..],
             )
         }
-    
+
         match reverse_template {
             Some(string) => self.build_template(&string, Some(&template_data)),
             None => template_data,
         }
     }
 
-
-    fn build_template(&self, template_name: &String, reverse_template_content: Option<&String>) -> String{
-
-        self.build(&format!("{}{}{}", self.template_directory, template_name, ".html"), reverse_template_content)
+    fn build_template(
+        &self,
+        template_name: &str,
+        reverse_template_content: Option<&str>,
+    ) -> String {
+        self.build(
+            &format!("{}{}{}", self.template_directory, template_name, ".html"),
+            reverse_template_content,
+        )
     }
 
-    fn build_article(&self, article_name: &String) -> String {
-
-        self.build(&format!("{}{}{}", self.article_directory, article_name, ".html"), None)
+    fn build_article(&self, article_name: &str) -> String {
+        self.build(
+            &format!("{}{}{}", self.article_directory, article_name, ".html"),
+            None,
+        )
     }
-    
-    // fn build_article(&self, article_name: &String) -> String {
-    //     println!("Desires to build article {:#?}", article_name);
-    //     let mut article_data =
-    //         read_to_string(format!("{}{}{}", self.article_directory, article_name, ".html")).unwrap();
-    
-    //     let first_line = article_data.lines().next().unwrap();
-    
-    //     let reverse_template: Option<String>;
-    //     if first_line.starts_with("*") {
-    //         reverse_template = Some(first_line.strip_prefix("*").unwrap().to_string());
-    //         println!(
-    //             "{} wants to be wrapped in {}",
-    //             article_name,
-    //             reverse_template.clone().unwrap()
-    //         );
-    //         article_data = article_data.replace(first_line, "");
-    //     } else {
-    //         reverse_template = None;
-    //     }
-    
-    //     while article_data.contains("!{") {
-    //         let parsed = self.parse_command(&article_data, "!{*}");
-    
-    //         article_data.replace_range(
-    //             parsed.start_index..(parsed.start_index + parsed.total_length),
-    //             &self.expand_bang(&parsed.name[..], parsed.arguments)[..],
-    //         );
-    //     }
-    
-    //     while article_data.contains("${") {
-    //         let parsed = self.parse_command(&article_data, "${*}");
-    //         article_data.replace_range(
-    //             parsed.start_index..(parsed.start_index + parsed.total_length),
-    //             &self.build_template(&parsed.name, None)[..],
-    //         );
-    //     }
-    
-    //     match reverse_template {
-    //         Some(string) => self.build_template(&string, Some(&article_data)),
-    //         None => article_data,
-    //     }
-    // }
-    
-    fn parse_command(&self, data: &String, command_format: &str) -> Command {
+
+    fn parse_command(&self, data: &str, command_format: &str) -> TemplateCommand {
         let mut split_command = command_format.split("*");
         let command_prefix = split_command.next().unwrap();
         let command_suffix = split_command.next().unwrap();
-    
+
         let index = data.find(command_prefix).unwrap();
-    
+
         let mut char_iterator = data.chars().peekable();
         char_iterator.nth(index + (command_prefix.len() - 1));
-    
+
         let mut command_name = String::new();
         while char_iterator.peek().is_some() {
             let next = char_iterator.next();
@@ -197,14 +242,14 @@ impl<'a> Compiler<'a>{
             };
             command_name.push(next.unwrap())
         }
-    
-        println!("Parsed out command {}", command_name);
-    
+
+        // println!("Parsed out command {}", command_name);
+
         let mut arguments: Vec<String> = Vec::new();
         let mut argument_size_in_text: usize = 0;
-    
+
         while char_iterator.next().unwrap_or('_') == '[' {
-            println!("Bang also has arguments!");
+            // println!("Bang also has arguments!");
             let mut argument = String::new();
             while char_iterator.peek().is_some() {
                 let next = char_iterator.next();
@@ -213,12 +258,12 @@ impl<'a> Compiler<'a>{
                 };
                 argument.push(next.unwrap())
             }
-            println!("Which is {:?}", argument);
+            // println!("Which is {:?}", argument);
             argument_size_in_text += 2 + argument.len();
             arguments.push(argument);
         }
-    
-        return Command {
+
+        return TemplateCommand {
             arguments: arguments,
             start_index: index,
             total_length: command_prefix.len()
@@ -230,7 +275,7 @@ impl<'a> Compiler<'a>{
     }
 }
 
-struct Command {
+struct TemplateCommand {
     name: String,
     arguments: Vec<String>,
     start_index: usize,
--- src/main.rs
+++ src/main.rs
@@ -8,56 +8,31 @@
 
 // use actix_files as fs;
 use actix_web::{App, HttpServer};
-use fs_extra::{self, dir::CopyOptions};
-use notify::{Watcher, RecursiveMode, watcher};
+use notify::{watcher, RecursiveMode, Watcher};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 mod compiler;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use compiler::Compiler;
+    use compiler;
 
-    let compiler = Compiler {
+    let compiler: compiler::Compiler = compiler::Compiler {
         article_directory: "src/articles/",
         template_directory: "./src/templates/",
     };
 
     let mut args = std::env::args();
     args.next();
-    args.for_each(|e| {
-        if e == "all" {
-            std::fs::read_dir(compiler.article_directory)
-                .unwrap()
-                .for_each(|file_result| {
-                    if file_result.is_ok() {
-                        let file_name = file_result.unwrap().file_name();
-                        match file_name.to_string_lossy().strip_suffix(".html") {
-                            Some(filename) => compiler.compile(&filename.to_string()),
-                            None => {}
-                        }
-                    }
-                })
-        } else {
-            compiler.compile(&e);
-        }
-    });
 
-    let copy_options = CopyOptions {
-        overwrite: true,
-        skip_exist: false,
-        content_only: true,
-        ..CopyOptions::new()
-    };
-    fs_extra::dir::copy("./src/public", "./built", &copy_options)
-        .expect("Could not copy contents of ./src/public");
+    compiler.interpret_arguments(&mut args);
 
-    let result = HttpServer::new(|| {
+    HttpServer::new(|| {
         App::new()
             .service(actix_files::Files::new("/public", "./src/public/"))
             .service(actix_files::Files::new("/", "./built"))
     })
-    .bind("192.168.1.244:8080")?
+    .bind("192.168.1.4:8080")?
     .bind("127.0.0.1:8080")?
     .run();
 
@@ -71,31 +46,27 @@ async fn main() -> std::io::Result<()> {
     // Add a path to be watched. All files and directories at that path and
     // below will be monitored for changes.
     watcher
-        .watch("src/articles/", RecursiveMode::Recursive)
+        .watch(compiler.article_directory, RecursiveMode::Recursive)
         .unwrap();
 
     loop {
         match rx.recv() {
-            Ok(event) => {
-                match event {
-                    notify::DebouncedEvent::NoticeWrite(pathbuf) => {
-                        let article_name = pathbuf.file_name().unwrap().to_str().unwrap();
-                        let article_name = String::from(article_name);
-                        let article_name = article_name.strip_suffix(".html");
-                        match article_name {
-                            Some(name) => {
-                                println!("Detected change for {:?}, compiling", name);
-                                compiler.compile(&String::from(name));
-                            },
-                            None => eprintln!("Changed article did not end in .html")
+            Ok(event) => match event {
+                notify::DebouncedEvent::NoticeWrite(pathbuf) => {
+                    let article_name = pathbuf.file_name().unwrap().to_str().unwrap();
+                    let article_name = String::from(article_name);
+                    let article_name = article_name.strip_suffix(".html");
+                    match article_name {
+                        Some(name) => {
+                            println!("Detected change for {:?}, compiling", name);
+                            compiler.compile(name);
                         }
-                    },
-                    _ => {}
+                        None => eprintln!("Changed article did not end in .html"),
+                    }
                 }
+                _ => {}
             },
             Err(e) => println!("watch error: {:?}", e),
         }
     }
-
-    return result.await;
 }
